6844b02fe6979cace9b7129876198777d6cc1376
Patrice Philetas <patricephiletas@Host-001.lan>
2026-02-23 20:35:12 +0100

    travail deploiement / ajout colonne url dans SQL

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -13,6 +13,11 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@5.4.296/build/pdf.
 
 const CLOUDINARY_BASE = "https://res.cloudinary.com/dpnudoyxb/image/upload";
 
+const CLOUD_NAME = "dpnudoyxb";
+
+const buildPdfUrl = (publicId: string) =>
+  `https://res.cloudinary.com/${CLOUD_NAME}/image/upload/${publicId}.pdf`;
+
 export default function App() {
   const [partitions, setPartitions] = useState<Partition[]>([]);
   const [search, setSearch] = useState("");
